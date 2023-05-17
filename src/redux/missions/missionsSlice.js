aee8c4de55fe05bb2d8e67a535ba60510e7368fe
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-17 18:16:56 +0000

    Fetch data from API
    Add extraReducers with a builder argument.

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  missions: [],
+  status: 'idle',
+  error: null,
+};
+
+const URL = 'https://api.spacexdata.com/v3/missions';
+
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    const response = await axios.get(URL);
+    return response.data;
+  },
+);
+
+export const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.missions = action.payload;
+      })
+      .addCase(fetchMissions.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default missionsSlice.reducer;
