902fb484b3e240f856267c8cc01c3952c5d0e1b1
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-18 19:47:33 +0000

    Update MyProfile page to include active mission list

--- src/components/Pages/MyProfile.js
+++ src/components/Pages/MyProfile.js
@@ -3,27 +3,27 @@ import MissionProfile from '../activeMission';
 import Navbar from '../navbar';
 
 const MyProfile = () => {
-  const rockets = useSelector((state)=>(state.rockets.rockets))
+  const rockets = useSelector((state) => (state.rockets.rockets));
   return (
     <>
       <Navbar />
-      <section className='MyprofileContainer'>
-    <MissionProfile />
+      <section className="MyprofileContainer">
+        <MissionProfile />
         <div className="RocketProfile">
-        <h1 className="MyRocket">My Rockets</h1>
-      <ul className='RocketUl'>
-      {
+          <h2 className="MyRocket">My Rockets</h2>
+          <ul className="RocketUl">
+            {
         rockets
-        .filter((element) => element.rocketStatus)
-        .map((rocket)=>(
-          <li className='RocketNames' key={rocket.id}>{rocket.RocketName}</li>
+          .filter((element) => element.rocketStatus)
+          .map((rocket) => (
+            <li className="RocketNames" key={rocket.id}>{rocket.RocketName}</li>
           ))
         }
-        </ul>
+          </ul>
         </div>
       </section>
     </>
- 
+
   );
 };
 
