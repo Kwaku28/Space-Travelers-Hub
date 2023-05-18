f0842bd135f62189a3d60e656d62b167d2118d8e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-05-18 16:17:18 +0000

    Add function to change the state of the mission actions

--- src/Redux/missions/missionsSlice.js
+++ src/Redux/missions/missionsSlice.js
@@ -1,4 +1,8 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {
+  createAsyncThunk,
+  createSlice,
+  isRejectedWithValue,
+} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
@@ -12,8 +16,12 @@ const URL = 'https://api.spacexdata.com/v3/missions';
 export const fetchMissions = createAsyncThunk(
   'missions/fetchMissions',
   async () => {
-    const response = await axios.get(URL);
-    return response.data;
+    try {
+      const response = await axios.get(URL);
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
   },
 );
 
@@ -21,7 +29,24 @@ const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {
-
+    joinMission: (state, action) => {
+      const missionId = action.payload;
+      state.missions = state.missions.map((mission) => {
+        if (mission.mission_id === missionId) {
+          return { ...mission, reserved: true };
+        }
+        return mission;
+      });
+    },
+    leaveMission: (state, action) => {
+      const missionId = action.payload;
+      state.missions = state.missions.map((mission) => {
+        if (mission.mission_id === missionId) {
+          return { ...mission, reserved: false };
+        }
+        return mission;
+      });
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -38,5 +63,7 @@ const missionsSlice = createSlice({
       });
   },
 });
+
+export const { joinMission, leaveMission } = missionsSlice.actions;
 
 export default missionsSlice.reducer;
